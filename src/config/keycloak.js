6f6179db652593ef610d441c8bb23539d76a894e
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-08 19:46:13 +0100

    Deployed

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -5,6 +5,7 @@ const keycloakConf ={
 	'serverUrl': 'https://sso-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/auth/',
 	'sslRequired': 'all requests',
 	'resource': 'wavers-sso',
+	'redirectUri': 'https://oauth-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/api/v1/auth/redhat-sso',
 	'credentials': {
 	  'secret': '5affe59f-18e7-41e0-ab62-dca4f7567169'
 	},
