40eddd0ed5524d809bd39f760622e9058c79ea7a
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-09 10:48:01 +0100

    Deployed

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -1,15 +1,15 @@
 // Redhat SSO server config
-const keycloakConf ={
-	'realm': 'climatewavers',
-	'clientId': 'wavers-sso',
-	'serverUrl': 'https://sso-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/auth/',
-	'sslRequired': 'all',
-	'resource': 'wavers-sso',
-	'credentials': {
-	  'secret': '5affe59f-18e7-41e0-ab62-dca4f7567169'
+const keycloakConf = {
+	"realm": "climatewavers",
+	"auth-server-url": "https://sso-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/auth/",
+	"ssl-required": "all",
+	"resource": "wavers-sso",
+	"verify-token-audience": true,
+	"credentials": {
+	  "secret": "5affe59f-18e7-41e0-ab62-dca4f7567169"
 	},
-	'confidentialPort': 0,
-	'policyEnforcer': {},
+	"confidential-port": 0,
+	"policy-enforcer": {}
   }
 
 
