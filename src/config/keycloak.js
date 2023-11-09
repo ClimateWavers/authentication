9c1ac733eed04c3aea6c9378273c847ce5828f1a
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-09 11:55:44 +0100

    Deployed

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -4,12 +4,9 @@ const keycloakConf = {
 	"auth-server-url": "https://sso-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/auth/",
 	"ssl-required": "external",
 	"resource": "wavers-sso",
-	"credentials": {
-	  "secret": "5affe59f-18e7-41e0-ab62-dca4f7567169"
-	},
-	"confidential-port": 0,
-	"policy-enforcer": {}
+	"public-client": true,
+	"verify-token-audience": true,
+	"use-resource-role-mappings": true,
+	"confidential-port": 0
   }
-
-
 module.exports = keycloakConf;
