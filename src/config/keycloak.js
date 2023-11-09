8a5793ef16429b4a7b896e4f93b5e883c126be83
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-09 10:37:47 +0100

    Deployed

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -3,7 +3,7 @@ const keycloakConf ={
 	'realm': 'climatewavers',
 	'clientId': 'wavers-sso',
 	'serverUrl': 'https://sso-olagoldhackxx-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/auth/',
-	'sslRequired': 'all requests',
+	'sslRequired': 'all',
 	'resource': 'wavers-sso',
 	'credentials': {
 	  'secret': '5affe59f-18e7-41e0-ab62-dca4f7567169'
